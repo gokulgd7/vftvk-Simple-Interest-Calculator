dfa16f27b55a949e7474aa91349be791196eb0e4
gokulgd7 <gokuljana75@gmail.com>
2021-06-11 08:11:49 +0000

    changes made from theia lab

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+    if(principal<=0){
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = (principal*years*rate)/100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit "+principal+<br/>+
-    "at an interest rate of "+rate+<br/>+"You will receive an amount of "+interest+<br/>+"in the year "+year;
+    document.getElementById("result").innerHTML="If you deposit "+principal+"<br/>"+
+    "at an interest rate of "+rate+"%"+"<br/>"+"You will receive an amount of "+interest+"<br/>"+"in the year "+year;
 
-    
+   return false; 
 }
 function updateRate()
 {
     var rateval=document.getElementById("rate").value;
-    document.getElementById("rate_val").innerHTML=rateval;
+    document.getElementById("rate_val").innerHTML=rateval+"%";
 }
         
